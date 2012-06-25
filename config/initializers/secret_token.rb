914607cc8d93a87692be76f5ba87e52929f8d1bf
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2012-06-25 13:54:50 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hype::Application.config.secret_token = 'f151553b832cc8e293f0e99012898ef237bcd4e0eb6656a6b7fbab5e654e07447f8478d8830c7013313c9f837691330908b6369da73da1a806dbd826a6bb1bfd'
