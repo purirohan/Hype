c0ca7fc792d2db5fb130cfe6a09d7c726492ed01
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2012-07-10 00:11:21 +0700

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,6 +68,18 @@ namespace :deploy do
   end
 end
 
+namespace :deploy do
+  namespace :assets do
+    desc "Precompile assets on local machine and upload them to the server."
+    task :precompile, roles: :web, except: {no_release: true} do
+      run_locally "bundle exec rake assets:precompile"
+      find_servers_for_task(current_task).each do |server|
+        run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+      end
+    end
+  end
+end
+
 # namespace :rvm do
 #   task :trust_rvmrc do
 #     run "rvm rvmrc trust #{release_path}"
@@ -75,3 +87,5 @@ end
 # end
 
 before "deploy", "check_production"#, 'rvm:install_rvm', "rvm:trust_rvmrc"
+
+after "deploy:cleanup"
