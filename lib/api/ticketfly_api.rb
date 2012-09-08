74aad0621aa08703f9182188a804b643cb45c327
Reki Hattori <reki@agencyprotocol.com>
2012-09-08 15:53:22 -0400

    strftime fix

--- lib/api/ticketfly_api.rb
+++ lib/api/ticketfly_api.rb
@@ -6,8 +6,8 @@ class TicketflyApi
   include ApiHelper
 
   def self.fetch
-    startDate = Week.this_week.today.strtime('%Y-%m-%d')
-    endDate = (Week.this_week.today + 1.week).strtime('%Y-%m-%d')
+    startDate = Week.this_week.today.strftime('%Y-%m-%d')
+    endDate = (Week.this_week.today + 1.week).strftime('%Y-%m-%d')
     orgs = '93,175,1417'
     query = "http://www.ticketfly.com/api/events/list.json?orgId=#{orgs}&fromDate=#{startDate}&thruDate=#{endDate}&maxResults=200"
     EventApi.get_http_response(query)
